eae5f7b5b26a9ba079fcc52604f4493a4c5de937
Dallas Lynn <dlynn@unitedweb.com>
2014-09-05 12:01:55 -0700

    actually add the Makefiles and dseq.c file

--- Makefile
+++ Makefile
@@ -1,14 +1,25 @@
-package = dseq
-version = 0.1
-tarname = $(package)
-distdir = $(tarname)-$(version)
-prefix  = /usr/local
-exec_prefix = $(prefix)
-bindir = $(exec_prefix)/bin
+# Makefile.  Generated from Makefile.in by configure.
 
-export prefix
-export exec_prefix
-export bindir
+package 	= dseq
+version		= 0.1
+tarname		= dseq
+distdir		= $(tarname)-$(version)
+
+prefix		= /usr/local
+exec_prefix	= ${prefix}
+bindir		= ${exec_prefix}/bin
+
+# package = dseq
+# version = 0.1
+# tarname = $(package)
+# distdir = $(tarname)-$(version)
+# prefix  = /usr/local
+# exec_prefix = $(prefix)
+# bindir = $(exec_prefix)/bin
+
+# export prefix
+# export exec_prefix
+# export bindir
 
 all clean install uninstall dseq:
 	cd src && $(MAKE) $@
@@ -17,6 +28,7 @@ dist: $(distdir).tar.gz
 
 distcheck: $(distdir).tar.gz
 	gzip -cd $(distdir).tar.gz | tar xvf -
+	cd $(distdir) && ./configure
 	cd $(distdir) && $(MAKE) all
 	cd $(distdir) && $(MAKE) DESTDIR=$${PWD}/_inst install
 	cd $(distdir) && $(MAKE) DESTDIR=$${PWD}/_inst uninstall
@@ -30,9 +42,17 @@ $(distdir).tar.gz:	$(distdir)
 
 $(distdir): FORCE
 	mkdir -p $(distdir)/src
-	cp Makefile $(distdir)
-	cp src/Makefile $(distdir)/src
+	cp configure.ac $(distdir)
+	cp configure $(distdir)
+	cp Makefile.in $(distdir)
+	cp src/Makefile.in $(distdir)/src
 	cp src/dseq.c $(distdir)/src
+
+Makefile: Makefile.in config.status
+		./config.status $@
+
+config.status: configure
+		./config.status --recheck
 
 FORCE:
 	-rm $(distdir).tar.gz >/dev/null 2>&1
--- src/dseq.c
+++ src/dseq.c
@@ -9,18 +9,17 @@ seq will go backwards only if increment is given.
 **** dseq "something parsable by getdate" number_of_days
 **** dseq "getdate() string" "get_date() string"
 **** --output-format="%Y%m%d"
-**** --backward (countdown instead of up)
 **** [[http://stackoverflow.com/questions/2655883/bash-shell-date-parsing-start-with-specifc-date-and-loop-through-each-day-in-mon][Stack Overflow Question on Bash Date Loops]]
 **** incorporate timers?
 **** tai64?
 **** hour/minute/second/ms/us instead of just dates
 
 seq 10 -> print 10 days into the future, starting from today   X
-seq -10 -> print 10 days into the past, starting from today
-seq '2014-07-11' 10 -> print 10 days starting with 2014-07-11  
+seq -10 -> print 10 days into the past, starting from today  X
+seq '2014-07-11' 10 -> print 10 days starting with 2014-07-11 
 seq '2014-07-11' '2014-07-17' -> print all days from 7-11-2014 to 7-17-2014, inclusive  X
-seq '2014-07-11' 4 '2015-07-11' -> print every fourth day between 2014-07-11 and 2015-07-11, inclusive, starting at 2014
-seq '2015-07-11' -4 '2014-07-11' -> print every fourth day between 2015-07-11 and 2014-07-11, inclusive, starting at 2015.
+seq '2014-07-11' 4 '2015-07-11' -> print every fourth day between 2014-07-11 and 2015-07-11, inclusive, starting at 2014 X
+seq '2015-07-11' -4 '2014-07-11' -> print every fourth day between 2015-07-11 and 2014-07-11, inclusive, starting at 2015.  X
 
 */
 
@@ -31,20 +30,23 @@ seq '2015-07-11' -4 '2014-07-11' -> print every fourth day between 2015-07-11 an
 #include <error.h>    /* error */
 #include <time.h>
 #include <string.h>
+#include <stdint.h>   /* SIZE_MAX */
+#include <error.h>    /* errno */
 
 #define PROGRAM_NAME "dseq"
 #define AUTHORS proper_name ("Dallas Lynn")
+#define ONE_DAY 24 * 60 * 60
 
-static bool equal_width;
+//extern int errno;
+
 static char const *separator;
 static long step;
+static char const terminator[] = "\n";
 
-// TODO: take an input format and an output format.
-// TODO: remove equal_width, it's pointless w/ explicit format
 static struct option const long_options[] =
 {
-  { "equal-width", no_argument, NULL, 'w'},
-  { "format", required_argument, NULL, 'f'},
+  { "input", required_argument, NULL, 'i'},
+  { "output", required_argument, NULL, 'o'},
   { "separator", required_argument, NULL, 's'},
   { "help", no_argument, NULL, 'h'},
   { "version", no_argument, NULL, 'v'},
@@ -53,16 +55,21 @@ static struct option const long_options[] =
 
 
 /* Check if FORMAT is a valid <function we use> format string */
-__attribute__ ((pure)) static bool
+static bool __attribute__ ((pure)) 
 is_valid_date_format (char const *fmt) {
   
   return true;
 }
 
 
-// TODO: should take a locale so we can take on on the comamnd line?
-/* Get the default format for the current locale */
-static char const *
+static size_t __attribute__ ((pure)) 
+format_size (char const *fmt, struct tm *t) {
+  return strftime(NULL, SIZE_MAX, fmt, t) + 1;
+}
+
+
+// TODO: test if this needs to be locale aware or strftime does it for me?
+static char const * __attribute__ ((const))
 get_default_format() {
   return "%Y-%m-%d";
 }
@@ -72,29 +79,32 @@ get_default_format() {
 // could be added individually.
 static void
 print_dates(struct tm start, struct tm end, long step, char const *fmt) {
-  // TODO: how to size given arbitrary fmt string?
-  char buf[11];
+  size_t buf_size = format_size(fmt, &start);
+  char buf[buf_size];
   time_t next;
+  time_t last = mktime(&end);
 
-  printf("step is %ld\n", step);
   for(;;) {
-    strftime(buf, 11, fmt, &start);
+    next = mktime(&start);
+
+    // TODO: make sure works with step '0' 
+    if((step > 0 && next > last) || (step < 0 && next < last))
+       break;
+  
+    strftime(buf, sizeof(buf), fmt, &start);
 
     // TODO: instead of printf should fwrite to stdout from a pre-allocated buffer with
     // the separator added?  seq does this.
     printf("%s%s", buf, separator);
     start.tm_mday += step;
-    next = mktime(&start);
-    strftime(buf, 11, fmt, localtime(&next));
-    
-    // TODO: only calculate mktime(&end) once and reuse.
-    if(next > mktime(&end))
-       break;
   }
+
+  printf("%s", terminator);
 }
 
-void
-usage(int status) {
+
+void __attribute__ ((noreturn))
+usage(int status) { 
   if (status != EXIT_SUCCESS) {
     //  TODO: gettext that and all dat text shit
     fprintf(stderr, "Try '%s --help' for more information.\n", PROGRAM_NAME);
@@ -105,18 +115,15 @@ usage(int status) {
   exit(status);
 }
 
-#define ISDIGIT(c) ((unsigned int) (c) - '0' <= 9)
 
 int main(int argc, char **argv) {
-  int optc;
-  time_t now;
+  int optc;                 
   struct tm start_tm, end_tm;
   memset(&start_tm, 0, sizeof(struct tm));
   memset(&end_tm, 0, sizeof(struct tm));
 
   char const *format_str = get_default_format();
-
-  equal_width = false;
+  char const *output_format = get_default_format();
   separator = "\n";
   step = 1;
 
@@ -124,29 +131,26 @@ int main(int argc, char **argv) {
      - set_program_name
      - setlocale
      - bindtextdomain / textdomain ?
-     - atexit (close_stdout)
+     - atexit (close_stdout) ?
   */
 
   while(optind < argc) {
-    /* TODO: use ISDIGIT macro instead? */
-    if(argv[optind][0] == '-' && isdigit(argv[optind][1])) {
-      printf("skiping minus sign\n");
+    if(argv[optind][0] == '-' && isdigit(argv[optind][1]))
       break;
-    }
 
-    optc = getopt_long(argc, argv, "+f:s:w", long_options, NULL);
+    optc = getopt_long(argc, argv, "+i:o:s", long_options, NULL);
     if(optc == -1)
       break;
     
     switch(optc) {
-    case 'f':
+    case 'i':
       format_str = optarg;
+      break;
+    case 'o':
+      output_format = optarg;
       break;
     case 's':
       separator = optarg;
-      break;
-    case 'w':
-      equal_width = true;
       break;
     case 'h':
       usage(EXIT_SUCCESS);
@@ -174,34 +178,41 @@ int main(int argc, char **argv) {
 
   /* if there is one arg it must be an integer and the implicit start is today */
   if(n_args == 1) {
-    now = time(NULL);
-    start_tm = *localtime(&now);
-    end_tm = start_tm;
-    // TODO: can tm_mday overflow?
-    // TODO: check return of strtoul or precheck to make sure a real int arg instead of eg. 10aaa
-    printf("arg: %s\n", argv[optind]);
-    end_tm.tm_mday += strtoul(argv[optind], NULL, 10);
+    // TODO: check return of strtol or precheck to make sure a real int arg instead of eg. 10aaa
+    // or xstrtol from gnulib
+    long offset = strtol(argv[optind], NULL, 10);
+    time_t now = time(NULL);
+    step = offset > 0 ? 1 : -1;
+
+    // TODO: overflow
+    time_t then = now + (offset * ONE_DAY) - (ONE_DAY * step);
+    localtime_r(&now, &start_tm);
+    localtime_r(&then, &end_tm);
   }
   
   /* if there are two args then the first should be a date and the second should
      be a date and we print the days between them */
   if(n_args == 2) {
-    printf("printing days between %s and %s\n", argv[optind], argv[optind+1]);
+    /* printf("printing days between %s and %s\n", argv[optind], argv[optind+1]); */
+    // TODO: check return of strptime.
     strptime(argv[optind], format_str, &start_tm);
     strptime(argv[optind+1], format_str, &end_tm);
+    if(mktime(&start_tm) > mktime(&end_tm))
+      step = -1;
   }
 
   /* if there are three args then it's start_date interval end_date */
   if(n_args == 3) {
-    printf("printing days between %s and %s in steps of %s\n", argv[optind], argv[optind+2],
-           argv[optind+1]);
+    /* printf("printing days between %s and %s in steps of %s\n", argv[optind], argv[optind+2], */
+    /*        argv[optind+1]); */
 
+    // TODO: check return of strptime.
     strptime(argv[optind], format_str, &start_tm);
     strptime(argv[optind+2], format_str, &end_tm);
     step = atol(argv[optind+1]);
   }
 
-  print_dates(start_tm, end_tm, step, format_str);
+  print_dates(start_tm, end_tm, step, output_format);
   
   exit(EXIT_SUCCESS);
 }
